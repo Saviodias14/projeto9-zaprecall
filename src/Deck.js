d6dd6dde715a6579542cee6828fd103dde8d0ca0
Savio Dias Viana <saviodiasviana@gmail.com>
2023-03-02 14:20:33 -0300

    Redução de states

--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,9 @@ import cards from "./listaCards"
 import Botoes from "./Botoes"
 import play from './assets/seta_play.png'
 import turn from './assets/seta_virar.png'
+import certo from './assets/icone_certo.png'
+import erro from './assets/icone_erro.png'
+import quase from './assets/icone_quase.png'
 import styled from "styled-components"
 import { useState } from "react"
 function criaArray(tamanho){
@@ -12,21 +15,20 @@ function criaArray(tamanho){
     return array
 }
 export default function (props) {
-    const [clicado, setClicado] = useState(cards)
-    const [vira, setVira] = useState(cards)
     const [texto, setTexto] = useState('')
     const [resposta, setResposta] = useState(criaArray(cards.length))
 
     function mostraPergunta(i) {
 
-        if (clicado[i]) {
-            let novoArray = [...clicado]
-            novoArray[i] = false
-            setClicado(novoArray)
+        if (!resposta[i]) {
+            let novoArray = [...resposta]
+            novoArray[i] = "clicado"
+            setResposta(novoArray)
             setTexto(cards[i].question)
-        } else {
-            console.log(vira)
-            setVira(clicado)
+        } else if(resposta[i]==='clicado') {
+            let novoArray = [...resposta]
+            novoArray[i] = "vira"
+            setResposta(novoArray)
             setTexto(cards[i].answer)
         }
     }
@@ -40,19 +42,36 @@ export default function (props) {
     function numeroDeRespostas(){
         let contador = 0
         resposta.find((r)=>{
-            if(r){
+            if(r==='verde'||r==='vermelho'||r==='laranja'){
                 contador++
             }
         })
         return contador
     }
 
+    function escolheImagem(i){
+        switch(resposta[i]){
+            case '':
+                return play
+            case 'clicado':
+                return turn
+            case 'verde':
+                return certo
+            case 'laranja':
+                return quase
+            case 'vermelho':
+                return erro
+            default:
+                return
+        }
+    }
+
     return (
         <Container>
             {cards.map((c, i) => <li key={c.answer}>
-                <Inicio  virar={vira[i]} começar={clicado[i]} resposta={resposta[i]}>
-                    <p>{(clicado[i] && vira[i])||resposta[i] ? `Pergunta ${i + 1}` : texto}</p>
-                    <img onClick={() => mostraPergunta(i)} src={clicado[i] ? play : turn} />
+                <Inicio resposta={resposta[i]}>
+                    <p>{!resposta[i] ||(resposta[i]!=='vira' && resposta[i]!=='clicado')? `Pergunta ${i + 1}` : texto}</p>
+                    <img onClick={() => mostraPergunta(i)} src={escolheImagem(i)} />
                     <Botoes pegaResposta={pegaResposta} indice={i} contador={props.contador(numeroDeRespostas())}></Botoes>
                 </Inicio></li>)}
         </Container>
@@ -71,8 +90,8 @@ img:hover{
 
 const Inicio = styled.div`
 width: 300px;
-height: ${props => props.começar || props.resposta ? '65px' : '131px'};
-background: ${props => props.começar || props.resposta ? '#FFFFFF' : '#ffffd4'};
+height: ${props => props.resposta==='vira' || props.resposta==='clicado'? '131px':'65px'};
+background: ${props => props.resposta==='vira' || props.resposta==='clicado'? '#ffffd4':'#FFFFFF' };
 box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
 border-radius: 5px;
 margin-bottom: 25px;
@@ -82,20 +101,20 @@ justify-content: space-between;
 padding-left: 15px;
 padding-right: 15px;
 box-sizing:border-box;
-position:${props => !props.começar && !props.resposta? 'relative' : ''};
+position:${props => props.resposta==='clicado' || props.resposta==='vira' ? 'relative' : ''};
 p{
-    position:${props => !props.começar && !props.resposta? 'absolute' : ''};
+    position:${props => props.resposta==='clicado' || props.resposta==='vira' ? 'absolute' : ''};
     left:15px;
     top:18px;
 }
 img{
-    position:${props => !props.começar && !props.resposta? 'absolute' : ''};
+    position:${props => props.resposta==='clicado' || props.respos==='vira' ? 'absolute' : ''};
     right:15px;
     bottom:6px;
-    display:${props => props.virar?'flex':'none'};
+    display:${props => props.resposta==='vira'?'none':'flex'};
 }
 div{
-    display: ${props => props.virar || props.resposta? 'none' : 'flex'};
+    display: ${props => props.resposta!=='vira'? 'none' : 'flex'};
     box-sizing: border-box;
     justify-content:space-between;
     align-items: center;
